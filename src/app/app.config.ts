6db383e9d03d89506bb87c048bc8a455adf5cdd7
Udo Leimberger <udo@trollsolutions.com>
2025-01-08 07:23:39 -0500

    add oauth module and make app zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, provideExperimentalZonelessChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideOAuthClient} from 'angular-oauth2-oidc';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    //provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(),
+    provideOAuthClient(),
+    provideRouter(routes)]
 };
